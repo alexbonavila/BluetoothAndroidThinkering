fefae963613e7343745d1155833db749d7731fc4
alexbonavila <alexbonavila@iesebre.com>
2016-02-16 19:17:04 +0100

    comprovem i hi ha bluetooth i l'activem

--- app/src/main/java/com/iesebre/dam2/alexbonavila/bluetooth/MainActivity.java
+++ app/src/main/java/com/iesebre/dam2/alexbonavila/bluetooth/MainActivity.java
@@ -1,13 +1,57 @@
 package com.iesebre.dam2.alexbonavila.bluetooth;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REQUEST_ENABLE_BT = 1;
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        isBluetoothAbailable();
+        
+        isEnabled();
+
+    }
+
+    public void isBluetoothAbailable(){
+
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+        }else {
+            // Device supports Bluetooth
+            Toast.makeText(this, "Bluetooth is available", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void isEnabled(){
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        
+        isBluetoothAbailable();
     }
 }
